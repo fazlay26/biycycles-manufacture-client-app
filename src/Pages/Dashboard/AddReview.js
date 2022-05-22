4f8f2a8441ad123e5bf4df62fe6efdd6444d095a
fazlay26 <rabboi007@gmail.com>
2022-05-23 01:00:24 +0600

    addreview-done

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const AddReview = () => {
+    const [user] = useAuthState(auth)
+    const handleReview = e => {
+        e.preventDefault()
+        const review = {
+            name: user?.displayName,
+            ratings: e.target.ratings.value,
+            review: e.target.review.value
+        }
+        fetch('http://localhost:5000/addreview', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(review)
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
     return (
         <div>
-            <h1>AddReview</h1>
+            <h1>Please Add you Valuable FeedBack</h1>
+            <form onSubmit={handleReview} className='flex justify-center'>
+                <div>
+                    <div class="form-control w-full max-w-xs">
+                        <label class="label">
+                            <span class="label-text">Ratings</span>
+                        </label>
+                        <input name='ratings' min='0' max='5' type="number" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div class="form-control w-full max-w-xs">
+                        <label class="label">
+                            <span class="label-text">Ratings</span>
+                        </label>
+                        <input name='review' min='0' max='5' type="text" placeholder="Enter Your FeedBack" class="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <input value='add' type="submit" placeholder="Enter Your FeedBack" class="btn w-full max-w-xs my-2" />
+                </div>
+            </form>
         </div>
     );
 };
--- src/Pages/HomePage/SingleReview.js
+++ src/Pages/HomePage/SingleReview.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -6,29 +6,12 @@ import { Pagination } from "swiper";
 import './SingleReview.css'
 
 const SingleReview = () => {
-    // const reviews = [
-    //     {
-    //         _id: 1,
-    //         name: 'Winson Herry',
-    //         location: 'CalihtmlFornia',
-    //         description: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
-
-    //     },
-    //     {
-    //         _id: 2,
-    //         name: 'Winson Herry',
-    //         location: 'CalihtmlFornia',
-    //         description: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
-
-    //     },
-    //     {
-    //         _id: 3,
-    //         name: 'Winson Herry',
-    //         location: 'CalihtmlFornia',
-    //         description: 'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
-    //     },
-    // ]
-
+    const [reviews, setReviews] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/review')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
     return (
         <>
             <Swiper
@@ -112,24 +95,31 @@ const SingleReview = () => {
                         </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide>
-                    <div className="card lg:max-w-lg bg-base-100 shadow-xl">
-                        <div className="card-body">
-                            <h2 className="card-title">It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content</h2>
-                        </div>
-                        <div className=''>
-                            <div className="avatar">
-                                <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 mr-10">
-                                    <img src='https://prestashoptheme.in/prs/PRS713/modules/tt_testimonialslider/views/img/testimonial_img1.jpg' alt='' />
+
+                {
+                    reviews.map(r => <>
+                        <SwiperSlide>
+                            <div className="card lg:max-w-lg bg-base-100 shadow-xl mr-4">
+                                <div className="card-body">
+                                    <h2 className="card-title">{r.review}</h2>
+                                </div>
+                                <div className=''>
+                                    <div className="avatar">
+                                        <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 mr-10">
+                                            <img src='https://prestashoptheme.in/prs/PRS713/modules/tt_testimonialslider/views/img/testimonial_img1.jpg' alt='' />
+                                        </div>
+                                    </div>
+                                    <div>
+
+                                        <h4>{r.name}</h4>
+                                        <h4>{r.ratings}</h4>
+                                    </div>
                                 </div>
                             </div>
-                            <div>
-                                <h1>Winson Herry</h1>
-                                <h4>CalihtmlFornia</h4>
-                            </div>
-                        </div>
-                    </div>
-                </SwiperSlide>
+                        </SwiperSlide>
+                    </>)
+                }
+
 
             </Swiper>
         </>
