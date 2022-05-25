471e8bc71a442fcb8c7edb519bd663b39d878c4c
fazlay26 <rabboi007@gmail.com>
2022-05-25 20:09:11 +0600

    my-order-delete-done

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,39 +1,45 @@
 import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import MyOrderTable from './MyOrderTable';
 
 const MyOrders = () => {
     const [user] = useAuthState(auth)
     const navigate = useNavigate()
-    console.log(user);
+    const [orderModal, setOrderModal] = useState(null)
     const [myorder, setMyOrder] = useState([])
+
+    console.log(user);
+
     //console.log(myorder);
     useEffect(() => {
 
-        fetch(`http://localhost:5000/myorder?customerEmail=${user?.email}`, {
-            method: 'GET',
-            headers: {
-                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-            }
-        })
-            .then(res => {
-                if (res.status === 401 || res.status === 403) {
-
-                    signOut(auth)
-                    localStorage.removeItem('accessToken')
-                    navigate('/')
-
+        if (user) {
+            fetch(`http://localhost:5000/myorder?customerEmail=${user?.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
                 }
+            })
+                .then(res => {
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth)
+                        localStorage.removeItem('accessToken')
+                        navigate('/')
+                    }
 
-                return res.json()
-            })
-            .then(data => {
-                console.log(data);
-                setMyOrder(data)
-            })
-    }, [user?.email, navigate])
+                    return res.json()
+                })
+                .then(data => {
+                    console.log(data);
+                    setMyOrder(data)
+                })
+        }
+    }, [user, navigate])
+
+
     return (
         <div>
             <h1>{user?.displayName} Has:{myorder.length} Orders</h1>
@@ -49,35 +55,28 @@ const MyOrders = () => {
                             <th>payment</th>
                             <th>Paid</th>
                             <th>TransictionId</th>
+                            <th></th>
+
 
                         </tr>
                     </thead>
                     <tbody>
+
                         {
-                            myorder.map(order => <>
-                                <tr>
-                                    <th>{order.product}</th>
-                                    <td>{order.customerName}</td>
-                                    <td>{order.customerEmail}</td>
-                                    <td>{order.orderQuantity}</td>
-                                    <td>{(order.pricePerUnit && !order.paid) && <Link to={`/dashboard/payment/${order._id}`}><button className='btn btn-xs btn-success'>Pay</button></Link>}</td>
-                                    <td>{(order.pricePerUnit && order.paid) && <span className='text-success'>paid</span>}</td>
-                                    <td>{order.transictionId}</td>
-                                    {
-                                        !order.paid && <td><button className='btn btn-xs'>Delete</button></td>
-                                    }
-
-                                    {/* <button class="btn btn-xs">delete</button> */}
-                                </tr>
-                            </>)
+                            myorder.map(order => <MyOrderTable key={order._id}
+                                order={order}
+                                setOrderModal={setOrderModal}
+                                orderModal={orderModal}
+                            >
+                            </MyOrderTable>)
                         }
-
-
-
 
                     </tbody>
                 </table>
             </div>
+
+
+
         </div>
     );
 };
